2e7f27f5f83a51b50149b9b676b2766e9cee6585
Anderson Mendes <developer.andersonmendes@gmail.com>
2021-10-21 16:51:57 -0300

    Aula 15 - finalizada

--- data-biding/src/app/app.module.ts
+++ data-biding/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AlertModule } from 'ngx-bootstrap/alert';
 import { InputPropertyComponent } from './input-property/input-property.component';
 import { OutputPropertyComponent } from './output-property/output-property.component';
+import { CicloComponent } from './ciclo/ciclo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
     InputPropertyComponent,
-    OutputPropertyComponent
+    OutputPropertyComponent,
+    CicloComponent
   ],
   imports: [
     BrowserModule,
