0eed2bf4c5e12ced3457c74d158c7c58db9c638a
Anderson Mendes <developer.andersonmendes@gmail.com>
2021-11-19 06:19:43 -0300

    Aula 29 - Finalizada

---
+++ diretivas/src/app/diretiva-ngclass/diretiva-ngclass.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngclass',
+  templateUrl: './diretiva-ngclass.component.html',
+  styleUrls: ['./diretiva-ngclass.component.css']
+})
+export class DiretivaNgclassComponent implements OnInit {
+
+  constructor() { }
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit(): void {
+  }
+
+}
