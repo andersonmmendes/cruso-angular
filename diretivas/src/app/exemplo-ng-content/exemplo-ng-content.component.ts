a60f37e0a605e30eda2d08bdeb31199538da0507
Anderson Mendes <developer.andersonmendes@gmail.com>
2021-11-22 21:45:23 -0300

    Aula 32 - Finalizada

---
+++ diretivas/src/app/exemplo-ng-content/exemplo-ng-content.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-ng-content',
+  templateUrl: './exemplo-ng-content.component.html',
+  styleUrls: ['./exemplo-ng-content.component.css']
+})
+export class ExemploNgContentComponent implements OnInit {
+
+  
+
+  constructor() { }
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit(): void {
+  }
+
+}
